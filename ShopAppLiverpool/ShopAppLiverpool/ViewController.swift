579851da8154f90816aa02000ee458e87dfaca00
Cesar Cabrera <itachi.upiita@gmail.com>
2020-07-25 09:09:04 -0500

    Populate repo

---
+++ ShopAppLiverpool/ShopAppLiverpool/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShopAppLiverpool
+//
+//  Created by Luis Fernando Bustos Ramírez on 25/07/20.
+//  Copyright © 2020 com.gastandoTenis.ShopAppLiverpool. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
